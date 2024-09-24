eaf49aee1e25e7bbc2c30f854da48c877a9db452
flowswif <121287406+flowswif@users.noreply.github.com>
2024-09-24 15:23:41 +0500

    CargoManager.cs

--- Assets/Scripts/CargoManager.cs
+++ Assets/Scripts/CargoManager.cs
@@ -12,87 +12,86 @@ public class CargoManager : MonoBehaviour
     public float MaxCenterOfGravity = 18.64f; // Максимальное значение центра тяжести (м)
 
     public float MaxMomentOfForce = 5000f; // Максимальный момент силы для устойчивости (Н·м)
-    public float AirDensity = 1.225f; // Плотность воздуха (кг/м³)
-    public float DragCoefficient = 0.3f; // Коэффициент лобового сопротивления
 
-    public bool CanPlaceCargo(GameObject cargo, Vector3 position)
+    public bool CanPlaceCargoList(List<Cargo> cargoList, Vector3 startPosition)
     {
-        Vector3 size = cargo.transform.localScale;
-        float cargoVolume = size.x * size.y * size.z;
-        float cargoWeight = cargo.GetComponent<Cargo>().Weight;
-        float crossSectionalArea = size.x * size.z; // Площадь поперечного сечения
+        float totalVolume = 0;
+        float totalWeight = 0;
+        Vector3 totalCenterOfGravity = Vector3.zero;
 
-        // Проверка на выход за границы грузового отсека
-        if (position.x < 0 || position.y < 0 || position.z < 0 ||
-            position.x + size.x > Dimensions.x ||
-            position.y + size.y > Dimensions.y ||
-            position.z + size.z > Dimensions.z)
+        // Проход по каждому грузу в списке
+        foreach (var cargo in cargoList)
         {
-            return false;
-        }
+            Vector3 size = new Vector3(cargo.Length, cargo.Height, cargo.Width);
+            float cargoVolume = size.x * size.y * size.z;
+            float cargoWeight = cargo.Weight;
 
-        // Проверка на пересечение с уже размещенными грузами
-        foreach (var placedCargo in placedCargos)
-        {
-            if (Intersect(position, size, placedCargo.Position, placedCargo.Size))
+            // Проверка на выход за границы грузового отсека
+            if (startPosition.x < 0 || startPosition.y < 0 || startPosition.z < 0 ||
+                startPosition.x + size.x > Dimensions.x ||
+                startPosition.y + size.y > Dimensions.y ||
+                startPosition.z + size.z > Dimensions.z)
             {
                 return false;
             }
-        }
 
-        // Подсчет общего объема и массы всех размещенных грузов
-        float totalVolume = cargoVolume;
-        float totalWeight = cargoWeight;
-        Vector3 totalCenterOfGravity = position * cargoWeight; // Центр тяжести умножаем на массу
+            // Проверка на пересечение с уже размещенными грузами
+            foreach (var placedCargo in placedCargos)
+            {
+                if (Intersect(startPosition, size, placedCargo.Position, placedCargo.Size))
+                {
+                    return false;
+                }
+            }
 
-        foreach (var placedCargo in placedCargos)
-        {
-            totalVolume += placedCargo.Size.x * placedCargo.Size.y * placedCargo.Size.z;
-            totalWeight += placedCargo.Weight;
+            // Добавление массы и объема груза
+            totalVolume += cargoVolume;
+            totalWeight += cargoWeight;
+            totalCenterOfGravity += startPosition * cargoWeight; // Центр тяжести умножаем на массу
 
-            // Вклад каждого груза в общий центр тяжести
-            totalCenterOfGravity += placedCargo.Position * placedCargo.Weight;
-        }
+            // Проверка, не превышает ли общий объем и масса допустимые значения
+            if (totalVolume > MaxVolume || totalWeight > MaxWeight)
+            {
+                return false;
+            }
 
-        // Проверка, не превышает ли общий объем и масса допустимые значения
-        if (totalVolume > MaxVolume || totalWeight > MaxWeight)
-        {
-            return false;
-        }
+            // Вычисление нового центра тяжести
+            Vector3 newCenterOfGravity = totalCenterOfGravity / totalWeight;
 
-        // Вычисление нового центра тяжести
-        Vector3 newCenterOfGravity = totalCenterOfGravity / totalWeight;
+            // Проверка, находится ли центр тяжести в допустимом диапазоне
+            if (newCenterOfGravity.x < MinCenterOfGravity || newCenterOfGravity.x > MaxCenterOfGravity)
+            {
+                return false;
+            }
 
-        // Проверка, находится ли центр тяжести в допустимом диапазоне
-        if (newCenterOfGravity.x < MinCenterOfGravity || newCenterOfGravity.x > MaxCenterOfGravity)
-        {
-            return false;
-        }
+            // Проверка устойчивости (момент силы)
+            float momentOfForce = cargoWeight * startPosition.y; // Момент силы по оси Y
+            if (momentOfForce > MaxMomentOfForce)
+            {
+                return false;
+            }
 
-        // Проверка устойчивости (момент силы)
-        float momentOfForce = cargoWeight * (position.y); // Расстояние до центра тяжести - по оси Y
-        if (momentOfForce > MaxMomentOfForce)
-        {
-            return false;
-        }
-
-        // Проверка лобового сопротивления
-        float dragForce = 0.5f * DragCoefficient * AirDensity * crossSectionalArea;
-        if (dragForce > MaxMomentOfForce) // Можно сравнивать с моментом силы или другим критерием
-        {
-            return false;
+            // Смещение позиции для следующего груза
+            startPosition += new Vector3(0, size.y, 0); // Примерное смещение по высоте
         }
 
         return true;
     }
 
-    public void PlaceCargoInScene(GameObject cargoObject, Vector3 position, Vector3 size)
+    public void PlaceCargoListInScene(List<Cargo> cargoList, Vector3 startPosition)
     {
-        GameObject placedCargo = Instantiate(cargoObject);
-        placedCargo.transform.position = position + size / 2;
-        placedCargo.transform.localScale = size;
+        foreach (var cargo in cargoList)
+        {
+            Vector3 size = new Vector3(cargo.Length, cargo.Height, cargo.Width);
+            GameObject placedCargo = Instantiate(cargo.CargoObject);
+            placedCargo.transform.position = startPosition + size / 2;
+            placedCargo.transform.localScale = size;
 
-        // Добавление груза в список размещенных
-        placedCargos.Add(new PlacedCargo(position, size, cargoObject.GetComponent<Cargo>().Weight));
+            placedCargos.Add(new PlacedCargo(startPosition, size, cargo.Weight));
+
+            // Смещение позиции для следующего груза
+            startPosition += new Vector3(0, size.y, 0);
+        }
     }
 }
+
