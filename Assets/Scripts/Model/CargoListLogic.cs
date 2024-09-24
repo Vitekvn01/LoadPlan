bcaa8278d723a6eea7bc04ed36f1109f3f67dfd0
flowswif <121287406+flowswif@users.noreply.github.com>
2024-09-24 15:17:39 +0500

    CargoListLogic.cs

--- Assets/Scripts/Model/CargoListLogic.cs
+++ Assets/Scripts/Model/CargoListLogic.cs
@@ -1,24 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class CargoListLogic
 {
     private List<List<Cargo>> _allCargoLists = new List<List<Cargo>>();
 
     public List<List<Cargo>> AllCargoLists => _allCargoLists;
 
-    public void CreateCargoList(float length, float width, float height, float weight, string name, bool isTiering, bool isOnlyFloor,  int count)
+    public void CreateCargoList(float length, float width, float height, float weight, string name, bool isTiering, bool isOnlyFloor, int count)
     {
-        List<Cargo> CargoList = new List<Cargo>();
+        if (length <= 0 || width <= 0 || height <= 0 || weight <= 0 || count <= 0)
+        {
+            Debug.LogError("Параметры груза должны быть положительными числами.");
+            return;
+        }
+
+        List<Cargo> cargoList = new List<Cargo>();
 
         for (int i = 0; i < count; i++)
         {
             Cargo cargo = new Cargo(length, width, height, weight, name, isTiering, isOnlyFloor);
-            CargoList.Add(cargo);
+            cargoList.Add(cargo);
         }
 
-        _allCargoLists.Add(CargoList);
+        _allCargoLists.Add(cargoList);
     }
-
 }
